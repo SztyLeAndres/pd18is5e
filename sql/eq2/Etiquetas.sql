eb11f9b13d23fa30a40432cbd084eef111ffd8f2
Carlos Valencia <33180719+carlos8520@users.noreply.github.com>
2018-10-18 12:33:49 -0500

    Add files via upload

---
+++ sql/eq2/Etiquetas.sql
@@ -0,0 +1,6 @@
+CREATE TABLE Etiquetas(
+    IdEtiqueta int NOT NULL AUTO_INCREMENT,
+    NombreEtiqueta varchar(40) NOT NULL,
+    Descripcion varchar(255) NOT NULL,
+    PRIMARY KEY (IdEtiqueta)
+);
